c506841016a9bf2f09bd37932cca108125beac6e
matchodura <mat.chodura@gmai..com>
2020-12-07 22:38:37 +0100

    Added identity system

---
+++ OnlineRefrigerator/Areas/Identity/Data/AuthDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using OnlineRefrigerator.Areas.Identity.Data;
+
+namespace OnlineRefrigerator.Data
+{
+    public class AuthDbContext : IdentityDbContext<AppUser>
+    {
+        public AuthDbContext(DbContextOptions<AuthDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ OnlineRefrigerator/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OnlineRefrigerator.Areas.Identity.Data;
+using OnlineRefrigerator.Data;
+
+[assembly: HostingStartup(typeof(OnlineRefrigerator.Areas.Identity.IdentityHostingStartup))]
+namespace OnlineRefrigerator.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AuthDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AuthDbContextConnection")));
+
+                services.AddDefaultIdentity<AppUser>(options => {
+                         options.SignIn.RequireConfirmedAccount = false;
+                         options.Password.RequireLowercase = false;
+                         options.Password.RequireUppercase = false;
+                })
+
+                    .AddEntityFrameworkStores<AuthDbContext>();
+            });
+        }
+    }
+}
