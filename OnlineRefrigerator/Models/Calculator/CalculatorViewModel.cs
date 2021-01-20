454410870fd16066cc8b026c7d0a188274fc7e3c
matchodura <mat.chodura@gmail.com>
2021-01-20 18:57:16 +0100

    Refactor code in calculator controller and used models

--- OnlineRefrigerator/Models/Calculator/CalculatorViewModel.cs
+++ OnlineRefrigerator/Models/Calculator/CalculatorViewModel.cs
@@ -10,9 +10,7 @@ namespace OnlineRefrigerator.Models
 {
     public class CalculatorViewModel
     {
-
-        //public List<Ingredients> Ingredients { get; set; }
-
+           
         public Ingredients Ingredient { get; set; }
 
         public SelectList ServingTypes { get; set; }
@@ -25,51 +23,3 @@ namespace OnlineRefrigerator.Models
     }
 }
 
-
-        //public List<Recipes> Recipes { get; set; }
-
-
-        //public List<SelectListItem> IngredientsCategories { get; set; }
-        //public List<SelectListItem> RecipesCategories { get; set; }
-        //public List<SelectListItem> Servings { get; set; }
-
-
-        //public int SelectedType { get; set; }
-        //public int SelectedServing { get; set; }
-
-        //public int SelectedCategory { get; set; }
-
-        //public List<Results> Results { get; set; }
-
-
-        //public string CategoryName { get; set; }
-
-
-
-        //public string Selected_Glossary { get; set; }
-        //public List<SelectListItem> Glossary_List { get; set; }
-
-        //public CalculatorViewModel()
-        //{
-
-        //    Results = new List<Results>();
-        //}
-
-    
-
-//    public class Results
-//    {
-
-//        public int Id { get; set; }
-//        public string Name { get; set; }
-
-//        public decimal EnergyValues { get; set; }
-
-//        public int CategoryId { get; set; }
-
-//        public string CategoryName { get; set; }
-
-//    }
-
-   
-//}
