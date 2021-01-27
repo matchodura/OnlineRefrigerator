d6bbc0e36b0b76521880e5d849bc7fbf3e4f69cb
matchodura <mat.chodura@gmail.com>
2021-01-27 13:50:17 +0100

    Display text on top of carousel image

---
+++ OnlineRefrigerator/wwwroot/js/carousel-text-content.js
@@ -0,0 +1,30 @@
+﻿$(document).ready(function () {
+
+    //default starting value
+    $('.text-box-content').html("seks");
+
+    $('#myCarousel').on('slide.bs.carousel', function (e) {
+        var slideFrom = $(this).find('.active').index();
+        var slideTo = $(e.relatedTarget).index();
+        var id = slideTo;
+
+        
+        switch (id) {
+            case 0:
+                $('.text-box-content').html("aaaaa");
+                break;
+            case 1:
+                $('.text-box-content').html("bbbbb");
+                break;
+            case 2:
+                $('.text-box-content').html("ccccc");
+                break;
+            default:
+            //the id is none of the above
+        }
+    });
+
+
+});
+
+    
